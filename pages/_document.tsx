b84b452204c84f3dae649fc2de6426101e0ac66f
Jordi Talens <talensjr@gmail.com>
2021-05-06 09:36:48 +0200

    Cleanup and refactor styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,60 @@ import Document, {
   NextScript,
 } from 'next/document'
 
+const Fonts = () => (
+  <>
+    <link
+      rel="preload"
+      href="/fonts/crafty-girls/regular.ttf"
+      as="font"
+      crossOrigin="anonymous"
+    />
+
+    <link
+      rel="preload"
+      href="/fonts/open-sans/regular.ttf"
+      as="font"
+      crossOrigin="anonymous"
+    />
+    <link
+      rel="preload"
+      href="/fonts/open-sans/light.ttf"
+      as="font"
+      crossOrigin="anonymous"
+    />
+  </>
+)
+
+const Favicons = () => (
+  <>
+    <link
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="/favicons/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicons/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/favicons/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/favicons/safari-pinned-tab.svg"
+      color="#5bbad5"
+    />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
+  </>
+)
+
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext,
@@ -20,60 +74,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Fonts */}
-          <link
-            rel="preload"
-            href="/fonts/crafty-girls/regular.ttf"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link rel="stylesheet" href="/fonts/crafty-girls/style.css" />
-
-          <link
-            rel="preload"
-            href="/fonts/open-sans/regular.ttf"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/open-sans/light.ttf"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link rel="stylesheet" href="/fonts/open-sans/style.css" />
-
-          {/* end of Fonts */}
-
-          {/* Favicons */}
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicons/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="theme-color" content="#ffffff" />
-          {/* End of Favicons */}
+          <Fonts />
+          <Favicons />
         </Head>
-        <body>
+        <body className="font-main">
           <Main />
           <NextScript />
         </body>
