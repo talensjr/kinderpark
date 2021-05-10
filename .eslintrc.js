311358f94bcd08e071889d59260a62886a2eba15
Jordi Talens <talensjr@gmail.com>
2021-05-10 10:36:50 +0200

    Revisited eslint config. Added logo. Added i18n library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,42 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest', 'react', 'react-hooks', 'jsx-a11y'],
+  plugins: ['@typescript-eslint', 'jsx-a11y'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   env: {
-    'jest/globals': true,
-    node: true,
+    es6: true,
     browser: true,
+    node: true,
+    jest: true,
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
   rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
+    'react/react-in-jsx-scope': 0,
+    // "@typescript-eslint/explicit-function-return-type": 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
   },
 }
